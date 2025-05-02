144878a46e2d1aa9e13733aea153b9310f31b049
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2025-05-02 14:59:24 +0200

    gets max occurance

--- MyOwnSandbox.cs
+++ MyOwnSandbox.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections;
 
 namespace MyZtmDsa
 {
     class MyOwnSandbox
     {
         public static int Increment (int n) => n + 1;
+
+        public static int Mode (int[] numbers)
+        {
+            // int[] numbers = {0, 0, 1, 2, 2, 3, 1, 1, 0};
+
+            Array.Sort(numbers);
+
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            int maxOccurance = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[i];
+
+                if (dictionary.ContainsKey(current))
+                {
+                    dictionary[current] = dictionary[current] + 1;
+                } 
+                else 
+                {
+                    dictionary[current] = 1;
+                }
+
+                if (dictionary[current] > maxOccurance) maxOccurance = dictionary[current];
+            }
+
+            return maxOccurance;
+        }
     }
+    
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace MyZtmDsa
     {
         static void Main(string[] args)
         {
-            int showtime = MyOwnSandbox.Increment(0);
+            // int showtime = MyOwnSandbox.Increment(0);
+            int[] numbers = {0, 0, 1, 2, 2, 3, 1, 1, 0};
+            int mode = MyOwnSandbox.Mode(numbers);
 
-            Console.WriteLine(showtime);
+            Console.WriteLine(mode);
         }
     }
 }
